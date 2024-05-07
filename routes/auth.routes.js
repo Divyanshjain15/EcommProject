9e494b5e42f4db4c8b68034d19f0fb93093de53d
Divyanshjain15 <146543679+Divyanshjain15@users.noreply.github.com>
2024-05-07 13:14:46 +0530

    Add files via upload

---
+++ routes/auth.routes.js
@@ -0,0 +1,23 @@
+/**
+ * POST 127.0.0.1:8888/ecomm/api/v1/auth/signup
+ *
+ * I need to intercept this
+ */
+const authController = require("../controllers/auth.controller");
+const authMW = require("../middlewares/auth.mw");
+
+module.exports = (app) => {
+    app.post(
+        "/ecomm/api/v1/auth/signup", [authMW.verifySignUpBody],
+        authController.signup
+    );
+
+    /**
+     * route for
+     * POST 0.0.0.0:8888/ecomm/api/v1/auth/signin
+     */
+    app.post(
+        "/ecomm/api/v1/auth/signin", [authMW.verifySignInBody],
+        authController.signin
+    );
+};
